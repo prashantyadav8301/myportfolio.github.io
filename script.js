eea89261ab4a5656806184a2c978fb8299022efb
Prashant <prashant123.iimt@gmail.com>
2022-03-19 22:16:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Happy Coding");
+
+document.querySelector(".b").style.display = "none";
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".sidebar").classList.toggle("sidebar-go")
+    if (document.querySelector(".sidebar").classList.contains("sidebar-go")) {
+        document.querySelector(".a").style.display = "inline"
+        document.querySelector(".b").style.display = "none"
+    }
+    else {
+        document.querySelector(".a").style.display = "none"
+        setTimeout(() => {
+            document.querySelector(".b").style.display = "inline"
+        }, 300);
+    }
+})
+
